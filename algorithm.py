9f05245f301d31825b8fb6430ee2098af5849211
mirun akyuz <mirunakyuz@gmail.com>
2016-10-13 18:05:02 +0300

    changing bmi_result array

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,5 @@
 def bmi(weight_array, height_array,is_metric):
-    bmi = []
+    bmi_result = []
 
 
     #Comparing arrays' length
@@ -9,11 +9,11 @@ def bmi(weight_array, height_array,is_metric):
     for i in range(len(weight_array)):
 
         if(weight_array[i] <= 0 or height_array[i] <= 0 ):
-                bmi.append("incorrect cell value")
+                bmi_result.append("incorrect cell value")
                 continue
         elif((is_metric and ( weight_array[i] >= 600 or height_array[i] >= 400)) or
         ((is_metric == 0 ) and ( weight_array[i] >= 1200 or height_array[i] >= 150)) ):
-                bmi.append("incorrect cell value")
+                bmi_result.append("incorrect cell value")
                 continue
 	    #calculates each value's bmi                
         if(is_metric):    
@@ -23,12 +23,12 @@ def bmi(weight_array, height_array,is_metric):
 
         #appends result to the return obje- bmi(array)
         if( bmi_value <= 18):
-           bmi.append("underweight")
+           bmi_result.append("underweight")
         elif( bmi_value < 25):
-           bmi.append("normal")
+           bmi_result.append("normal")
         elif( bmi_value < 30):
-           bmi.append("overweight")
+           bmi_result.append("overweight")
         else:
-           bmi.append("obese")
+           bmi_result.append("obese")
 
-    return bmi
+    return bmi_result
